b813ba84f91ce1c6294cad29ddf5016d8e6e610b
Ayoub <aero.engineer1337@gmail.com>
2023-09-05 21:14:25 +0100

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i % 3 == 0 or i % 5 == 0:
+            if i % 3 == 0 and i % 5 != 0:
+                print("Fizz", end=' ')
+            elif i % 5 == 0 and i % 3 != 0:
+                print("Buzz", end=' ')
+            else:
+                print("FizzBuzz", end=' ')
+        else:
+            print("{}".format(i), end=' ')
