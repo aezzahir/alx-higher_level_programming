e6e3c9f6301b59125bf7c16f4affe03d5a044d92
Ayoub <aero.engineer1337@gmail.com>
2023-09-06 07:26:44 +0100

    task 11

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    str_cpy = ""
+
+    for i in range(len(str)):
+        if i != n:
+            str_cpy += str[i]
+    return str_cpy
+
+
