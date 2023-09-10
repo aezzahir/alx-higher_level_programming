4a9ec9f49916a6fcef1caba24994172b17e9b98e
Ayoub <aero.engineer1337@gmail.com>
2023-09-10 15:57:37 +0100

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [1, 2, 3, 9, 5],
+    [4, 5, 6, 6, 8],
+    [7, 8, 9, 0, 5],
+    [1, 0]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
