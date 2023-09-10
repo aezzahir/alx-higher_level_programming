628b96a5e313ed5f2214c5135ca9c4fb856cea0a
Ayoub <aero.engineer1337@gmail.com>
2023-09-10 15:35:17 +0100

    3. Print a list of integers... in reverse!

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,12 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    if not my_string:
-        pass
-    else:
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                new_string = new_string + i
-    return new_string
+    string_length = len(my_string)
+    i = 0
+    new_string = my_string[:]
+
+    for char in range(string_length):
+        if (my_string[char] == 'c' or my_string[char] == 'C'):
+            new_string = new_string[:(char - i)] + my_string[(char + 1):]
+            i += 1
+
+    return (new_string)
