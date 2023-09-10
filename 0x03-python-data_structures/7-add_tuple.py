194840e94cbdc4cdbd6517e07587d9c2a7d4fc76
Ayoub <aero.engineer1337@gmail.com>
2023-09-10 16:35:41 +0100

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = 0, 0
-    elif len(tuple_a) == 1:
+    my_tuple = ()
+    if len(tuple_a) == 1:
         tuple_a = tuple_a[0], 0
-    if len(tuple_b) == 0:
-        tuple_b = 0, 0
-    elif len(tuple_b) == 1:
+    elif len(tuple_a) == 0:
+        tuple_a = 0, 0
+    if len(tuple_b) == 1:
         tuple_b = tuple_b[0], 0
-    sum = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return sum
+    elif len(tuple_b) == 0:
+        tuple_b = 0, 0
+    my_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return my_tuple
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -2,7 +2,7 @@
 add_tuple = __import__('7-add_tuple').add_tuple
 
 tuple_a = (1, 1)
-tuple_b = (2, 2)
+tuple_b = (, 2)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
