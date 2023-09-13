6ba0ca90d178ddd4d3e5629cbb542abc2dbebfd2
Ayoub <aero.engineer1337@gmail.com>
2023-09-13 21:23:35 +0100

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for _list in matrix:
+        new_matrix.append(list(map(lambda x : x**2, _list)))
+    return (new_matrix)
