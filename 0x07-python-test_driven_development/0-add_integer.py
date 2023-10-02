c88b0793e03160e8f864fd316b6df019c972b518
Ayoub <aero.engineer1337@gmail.com>
2023-10-03 00:54:58 +0100

    defining a rectangle

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Function to add two numbers"""
+    return (a + b)
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module for rectangle"""
+
+
+class Rectangle:
+    """Empty class that defines a rectangle"""
+    pass
