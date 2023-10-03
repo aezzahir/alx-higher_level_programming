863134e70e720dfa1f0d95407d761b9ae134ca1e
Ayoub <aero.engineer1337@gmail.com>
2023-10-03 01:13:19 +0100

    defining a rectangle

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""this module defines a Rectangle with width and height"""
+
+
+class Rectangle:
+    """Class that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initialize rectangle with width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Return the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+    def __str__(self):
+        """Return string representation of the rectangle using #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        representation = []
+        for i in range(self.__height):
+            representation.append("#" * self.__width)
+        return "\n".join(representation)
+    def __repr__(self):
+        """Return string representation for eval"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
