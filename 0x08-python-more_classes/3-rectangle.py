f774b2632234da398e9ac664cf2e91c33175b0ed
Ayoub <aero.engineer1337@gmail.com>
2023-10-03 01:23:22 +0100

    defining a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,6 +47,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """Return string representation of the rectangle using #"""
         if self.__width == 0 or self.__height == 0:
