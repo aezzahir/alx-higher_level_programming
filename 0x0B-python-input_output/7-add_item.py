93b00b82a95ec034edb7a4327f5140530739ad29
Ayoub <aero.engineer1337@gmail.com>
2023-10-11 00:22:55 +0100

    6. Create object from a JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
-"""
-Write a script that adds all arguments to a Python list,
-and then save them to a file:
-"""
-
+"""A script that adds all arguments to a Python list, and then
+save them to a file:"""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+    try:
+        existing_list = load_from_json_file("add_item.json")
+    except Exception:
+        existing_list = []
+    for arg in (sys.argv[1:]):
+        existing_list.append(arg)
+    save_to_json_file(existing_list, "add_item.json")
