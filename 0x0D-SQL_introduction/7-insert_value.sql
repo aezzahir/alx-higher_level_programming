81075a7bdcde2475376562345f205fb496c8f377
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-11-14 23:54:31 +0100

    7. First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO first_tables VALUES (89, 'Best School');
+INSERT INTO first_tables(id, name) VALUES (89, 'Best School');
 
