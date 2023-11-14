0d40408659591bf1ed7b6587bb36423cb3a625d5
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-11-15 00:06:01 +0100

    Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+
