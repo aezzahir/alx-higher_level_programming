5a2f714b17b1f99141004e598bc4b77f18d20c58
a.ezzahiri <ayb.ezzahiri@gmail.com>
2023-12-12 12:46:40 -0800

    6 - square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const sqrt = require('./5-square');
+class Square extends sqrt {
+
+	charPrint(c){
+	if(!c) {
+		this.print();
+	}
+		else { 
+for (let j = 0; j < this.height; j++) {
+      for (let i = 0; i < this.width; i++) {
+        process.stdout.write(c);
+      }
+      process.stdout.write('\n');
+    }
+		}
+	}
+}
+module.exports = Square;
