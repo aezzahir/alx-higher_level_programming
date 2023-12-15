e6f575f210553eef4e7d96ab3e82873b2436529d
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-12-15 19:00:29 +0100

    6. First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,6 +16,6 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=True)
     def __init__(self, name):
         self.name = name
