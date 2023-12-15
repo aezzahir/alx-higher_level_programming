8121fb8dfe43f7e9d6769e9593e332ec3e77d669
Ayoub EZZAHIRI <aero.glowsky@gmail.com>
2023-12-15 20:16:17 +0100

    9. Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy import asc
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(asc(State.id))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
