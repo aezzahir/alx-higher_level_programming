9a021896a214d42fdb8df15fba6e9f70d4bd2c96
aezzahir <aero.glowsky@gmail.com>
2023-12-16 11:30:32 -0800

    16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,14 +12,13 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
-    states = session.query(State).join(State.cities).order_by(
-        State.id, City.id).all()
+    states = session.query(State).all()
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(f"    {city.id}: {city.name}")
-    session.close()
+            print("     {}: {}".format(city.id, city.name))
