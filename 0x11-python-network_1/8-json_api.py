a440abc848128d815a87d5034a7ac6374d19b7b7
aezzahir <aero.glowsky@gmail.com>
2024-01-07 22:47:38 +0100

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+8. Search API
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    data = {"q": letter}
+    r = requests.post(url, data=data)
+    try:
+        r_json = r.json()
+        if r_json:
+            print("[{}] {}".format(r_json.get("id"), r_json.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
