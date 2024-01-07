2dd73bbeab3ea7ade1de202ee960ea0177985474
aezzahir <aero.glowsky@gmail.com>
2024-01-07 15:25:58 +0100

    web stack debbuging

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -w "%{size_download}" -o /dev/null "$1" | { read size; echo "$size"; }
+curl -Ls "$1"
